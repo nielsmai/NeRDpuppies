602ca8c182ed270c77aef4b389a14a4958a12d27
Kateri Duranceau <kateri.duranceau@gmail.com>
2026-03-16 17:23:58 +0000

    Corrected Warp's up axis to match the urdf one (Z up, Warp is default Y up)

--- pupper_test.py
+++ pupper_test.py
@@ -1,41 +1,43 @@
 import warp as wp
 import warp.sim as wp_sim
 import warp.sim.render
+import numpy as np
 
+# 0. initialize warp simulator
 wp.init()
 device = "cuda" if wp.is_cuda_available() else "cpu"
 
 # 1. Setup Model
-builder = wp_sim.ModelBuilder()
-builder.default_shape_ke = 1e4
-builder.default_shape_kd = 250.0
-builder.default_shape_kf = 500.0
-builder.default_shape_margin = 0.01
+#builder = wp_sim.ModelBuilder()
+builder = wp_sim.ModelBuilder(up_vector=(0.0, 0.0, 1.0))        #change the up axis in Warp, to match the URDF one
+builder.default_shape_ke = 1e4          #contact stiffness
+builder.default_shape_kd = 250.0        #contact damping, 
+builder.default_shape_kf = 500.0        #fiction stiffness
+builder.default_shape_margin = 0.01     #collision margin (1cm, how close objects can get before collision detected)
 
-wp_sim.parse_urdf("urdf/standford_pupper.urdf", builder, floating=True)
+wp_sim.parse_urdf("urdf/standford_pupper.urdf", builder, floating=True) #load urdf into builder
+model = builder.finalize(device)    #uploads model to gpu, final
+model.ground = True                 #enable ground plane
 
-model = builder.finalize(device)
-model.ground = True
+# 2. Setup State 
+state_in = model.state()            #state inputs buffer
+state_out = model.state()           #tate outputs buffer
 
-# 2. Setup State
-state_in = model.state()
-state_out = model.state()
-
-state_in.body_q.numpy()[0] = [0.0,0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
-state_in.body_q.assign(state_in.body_q.numpy())
+q = state_in.body_q.numpy()  
+q[0] = [0.0, 0.3, 0.212, 0.0, 0.0, 0.0, 1.0] #sets the base link pose
+state_in.body_q.assign(q)                #assign position to warp tensor on GPU
 
 # 3. Initialize the Integrator
-integrator = wp_sim.XPBDIntegrator(iterations=10)
+integrator = wp_sim.XPBDIntegrator(iterations=10)               #create a stable physics solver
 
 # 4. Setup Renderer
-renderer = wp_sim.render.SimRenderer(model, "output4.usd", fps=60)
+renderer = wp_sim.render.SimRenderer(model, "outputs/o2.usd", fps=60)  #to visualize
 
 # 5. Simulation Loop
-state_in.body_qd.zero_()
+state_in.body_qd.zero_()        #set velocities to 0 to start
 
 # warm up — let the integrator accept the initial state
-import numpy as np
-for _ in range(10):
+for _ in range(10):                 #10 physics steps without rendering
     wp.sim.collide(model, state_in)
     integrator.simulate(model, state_in, state_out, dt=1.0/60.0)
     state_in, state_out = state_out, state_in
