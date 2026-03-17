c3812d670edd3db0e19c5dbef5099170f151fadd
kateri <kateri.duranceau@users.noreply.github.com>
2026-03-17 13:37:36 +0000

    Pushed the urdf test code

--- pupper_test.py
+++ pupper_test.py
@@ -15,32 +15,34 @@ builder.default_shape_kd = 250.0        #contact damping,
 builder.default_shape_kf = 500.0        #fiction stiffness
 builder.default_shape_margin = 0.01     #collision margin (1cm, how close objects can get before collision detected)
 
-wp_sim.parse_urdf("urdf/standford_pupper.urdf", builder, floating=True) #load urdf into builder
+wp_sim.parse_urdf("/teamspace/studios/this_studio/puppersim/puppersim/puppersim/data/pupper_v2a.urdf", builder, floating=True) #load urdf into builder
 model = builder.finalize(device)    #uploads model to gpu, final
-model.ground = True                 #enable ground plane
+model.ground = False               #enable ground plane
 
 # 2. Setup State 
 state_in = model.state()            #state inputs buffer
 state_out = model.state()           #tate outputs buffer
 
 q = state_in.body_q.numpy()  
-q[0] = [0.0, 0.3, 0.212, 0.0, 0.0, 0.0, 1.0] #sets the base link pose
+q[0] = [0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 1.0] #sets the base link pose
 state_in.body_q.assign(q)                #assign position to warp tensor on GPU
 
 # 3. Initialize the Integrator
-integrator = wp_sim.XPBDIntegrator(iterations=10)               #create a stable physics solver
+integrator = wp_sim.SemiImplicitIntegrator()             #create a stable physics solver
 
 # 4. Setup Renderer
-renderer = wp_sim.render.SimRenderer(model, "outputs/o2.usd", fps=60)  #to visualize
+renderer = wp_sim.render.SimRenderer(model, "outputs/o4.usd", fps=60)  #to visualize
 
 # 5. Simulation Loop
 state_in.body_qd.zero_()        #set velocities to 0 to start
 
-# warm up — let the integrator accept the initial state
-for _ in range(10):                 #10 physics steps without rendering
+# let the robot fall and settle before recording
+for i in range(200):
     wp.sim.collide(model, state_in)
-    integrator.simulate(model, state_in, state_out, dt=1.0/60.0)
+    integrator.simulate(model, state_in, state_out, dt=1.0/2000.0)
     state_in, state_out = state_out, state_in
+    q = state_in.body_q.numpy()
+    print(f"step {i:3d} | body z={q[0][2]:.4f} | RF_TOE z={q[13][2]:.4f}")
 
 # now run the real loop
 for i in range(100):
@@ -48,7 +50,7 @@ for i in range(100):
     wp.sim.collide(model, state_in)
     integrator.simulate(model, state_in, state_out, dt=1.0/60.0)
     state_in, state_out = state_out, state_in
-
+    q = state_in.body_q.numpy()
     renderer.begin_frame(i / 60.0)
     renderer.render(state_in)
     renderer.end_frame()
